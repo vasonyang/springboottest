8f2687dcf94309c75649de995b1c636b7e1aad87
杨瑞龙 <yangruilong@ultrapower.com.cn>
2018-01-09 14:07:55 +0800

    学习logback

--- testLog/src/main/java/com/test/logtest/service/impl/UserServiceImpl.java
+++ testLog/src/main/java/com/test/logtest/service/impl/UserServiceImpl.java
@@ -1,10 +1,10 @@
 package com.test.logtest.service.impl;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
-import org.assertj.core.util.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import com.google.gson.Gson;
 import com.test.logtest.bean.User;
@@ -13,8 +13,10 @@ import com.test.logtest.service.UserService;
 @Service
 public class UserServiceImpl implements UserService {
 
+  Logger logger = LoggerFactory.getLogger("testNameLog"); // 去logback-spring.xml 中查找  name 为  testNameLog 的logger 
   @Override
   public User getUserById(String id) {
+    logger.info("根据id获取用户服务");
     User user = new User();
     user.setId(id);
     user.setName("啊立刻就");
@@ -32,6 +34,7 @@ public class UserServiceImpl implements UserService {
 
   @Override
   public List<User> getUserByName(String name) {
+    logger.info("根据name获取用户服务");
     List<User> list = new ArrayList<>();
     for (int i = 0; i < 5; i++) {
       User user = new User();
@@ -56,7 +59,16 @@ public class UserServiceImpl implements UserService {
 //    JsonObject obj = new JsonObject();
 //    obj.addProperty("code", 200);
 //    obj.addProperty("result", "success");
+    
+
     return 1;
   }
 
+//  public static void main(String[] args) {
+//    String date = LocalDate.now().plusDays(1).toString();
+//    System.out.println(date);
+//    
+//    String date2 = LocalDate.now().minusYears(1).toString();
+//    System.out.println(date2);
+//  }
 }
